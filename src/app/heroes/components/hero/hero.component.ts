d8e8e01b96cfa253144e73e5723269e11c4a4c8c
Ignacio Merino <ignacio.merino@gmail.com>
2023-04-21 20:48:15 +0200

    Fin de seccion 4

---
+++ src/app/heroes/components/hero/hero.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+
+  public name: string = 'ironman';
+  public age: number = 45;
+
+  get capitalizedName():string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string {
+    return `${ this.name } - ${ this.age }`;
+//    return this.name +' - '+ this.age ;
+  }
+
+  changeHero():void {
+    this.name = 'Spiderman';
+  }
+
+  changeAge():void {
+    this.age = 30;
+  }
+
+  reset():void {
+    this.age = 45;
+    //this.name = 'ironman';
+
+    document.querySelector('h1')!.innerHTML = '<h1>Cambio desde Angular</h1>';
+
+    document.querySelectorAll('h1')!.forEach(element => {
+      element.innerHTML = '<h1>Desde Angular2</h1>';
+    })
+  }
+
+}
